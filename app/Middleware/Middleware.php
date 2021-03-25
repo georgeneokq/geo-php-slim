62221f650527ecf72cb292d87eeea56b9837950f
George <george.neokq@gmail.com>
2021-03-25 20:39:00 +0800

    Added get function to base middleware class

--- app/Middleware/Middleware.php
+++ app/Middleware/Middleware.php
@@ -11,4 +11,13 @@ class Middleware
     protected function encode($content) {
         return json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
+
+    /*
+     * Gets an array item, with default value of null
+     * @var $array array to retrieve value from
+     * @var $key array item to get
+     */
+    protected function get($body, $key) {
+        return isset($body[$key]) ? $body[$key] : null;
+    }
 }
