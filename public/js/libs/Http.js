e73d9e81ee430ebf7ee77fa044ce454b8218337d
George <george.neokq@gmail.com>
2021-03-30 16:54:50 +0800

    Added toFormData function in Http.js

--- public/js/libs/Http.js
+++ public/js/libs/Http.js
@@ -36,3 +36,19 @@ const Http2 = {
         return await response.json();
     }
 };
+
+function toFormData(object, formData = null) {
+    if(!formData) {
+        formData = new FormData();
+    }
+    for (let key in object) {
+        if(Array.isArray(object[key])) {
+            for(let array_item of object[key]) {
+                formData.append(key, array_item);
+            }
+        } else {
+            formData.append(key, object[key]);
+        }
+    }
+    return formData;
+}
